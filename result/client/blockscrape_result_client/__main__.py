2cdfa894a6dc7aa70b7020500d29b9cffdcb1d7b
larsjaeger <lars@jgr.one>
2023-09-09 11:39:29 +0200

    move to synchronous version of socketio client

--- result/client/blockscrape_result_client/__main__.py
+++ result/client/blockscrape_result_client/__main__.py
@@ -14,26 +14,24 @@ parsed_args = parser.parse_args()
 mining_server_url: str = parsed_args.mining_server_url
 output_dir: str = parsed_args.output_dir
 
-sio = socketio.AsyncClient(logger=True, engineio_logger=True)
+sio = socketio.Client(logger=True, engineio_logger=True)
 
 
 @sio.event
-async def connect():
-    await sio.emit("set_job", parsed_args.job_id)
+def connect():
+    sio.emit("set_job", parsed_args.job_id)
     print("set job_id to: ", parsed_args.job_id)
 
 
 @sio.on("task_result")
-async def compute_task_bundle(data: TaskResultSchema):
+def compute_task_bundle(data: TaskResultSchema):
     # write result to file
     with open(f"{output_dir}/{str(data.time)}", "w") as f:
         f.write(JSONEncoder().encode(data.model_dump_json()))
 
 
 if __name__ == "__main__":
-    asyncio.run(sio.connect(mining_server_url))
     try:
-        while True:
-            asyncio.run(sio.wait())
+        sio.connect(mining_server_url)
     except KeyboardInterrupt:
         print("exiting")
