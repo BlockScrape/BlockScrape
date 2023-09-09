b19a2ab453e5aeb8000b08d847381e7b70c06245
larsjaeger <lars@jgr.one>
2023-09-09 11:27:47 +0200

    fix sio init order

--- result/client/blockscrape_result_client/__main__.py
+++ result/client/blockscrape_result_client/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
 from json import JSONEncoder
+
 import socketio
 
 from schema import TaskResultSchema
@@ -13,8 +14,7 @@ parsed_args = parser.parse_args()
 mining_server_url: str = parsed_args.mining_server_url
 output_dir: str = parsed_args.output_dir
 
-sio = socketio.AsyncClient()
-asyncio.run(sio.connect(mining_server_url))
+sio = socketio.AsyncClient(logger=True, engineio_logger=True)
 
 
 @sio.event
@@ -23,7 +23,12 @@ async def connect():
 
 
 @sio.on("task_result")
-async def compute_task_bundle(data:TaskResultSchema):
+async def compute_task_bundle(data: TaskResultSchema):
     # write result to file
     with open(f"{output_dir}/{str(data.time)}", "w") as f:
         f.write(JSONEncoder().encode(data.model_dump_json()))
+
+
+if __name__ == "__main__":
+    asyncio.run(sio.connect(mining_server_url))
+    asyncio.run(sio.wait())
